8d64eb246560dc267cb8139940079164ce5eb8fc
Carlos Castañeda <carloscodvcf@gmail.com>
2025-05-26 01:28:49 +0200

    Creado un script para automatizar la creación de los archivos .html

---
+++ src/mjml-to-html.ps1
@@ -0,0 +1,19 @@
+# Rutas de las carpetas
+$mjmlFolder = ".\mjml"
+$htmlFolder = ".\html"
+
+# comprobación de carpeta de destino
+if (!(Test-Path $htmlFolder)) {
+    New-Item -ItemType Directory -Path $htmlFolder | Out-Null
+}
+
+# se listan los .mjml
+Get-ChildItem -Path $mjmlFolder -Filter *.mjml | ForEach-Object {
+    $mjmlFile = $_.FullName
+    $fileNameWithoutExt = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+    $htmlFile = Join-Path $htmlFolder "$fileNameWithoutExt.html"
+
+    # se convierte el archivo a .html, conservando el nombre
+    Write-Host "Convirtiendo $($_.Name) a HTML..." -ForegroundColor Cyan
+    mjml $mjmlFile -o $htmlFile
+}
