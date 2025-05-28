9e2912e1a625a762a4e07db8a8b0766b9bde8913
Carlos Castañeda <carloscodvcf@gmail.com>
2025-05-28 16:39:48 +0200

    actualización de EnviarCorreos.py

--- src/python/EnviarCorreos.py
+++ src/python/EnviarCorreos.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+Script name: EnviarCorreos.py
+Author: Carlos Castañeda
+Description: Script que automatiza el envío de templates .html por correo
+Version: 1.0
+"""
+
 import smtplib
+import sys
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -14,12 +22,20 @@ def leer_credenciales(ruta):
                 credenciales[clave.strip()] = valor.strip()
     return credenciales
 
+def comprobar_credenciales(ruta):
+    if not os.path.exists(ruta):
+        print(f"No se ha encontrado el archivo de credenciales en: {ruta}")
+        sys.exit(1)  # Si no se encuentra el archivo, se para el programa
+    else:
+        print(f"Archivo de credenciales encontrado en: {ruta}")
+
 # Ruta del script actual
 ruta_base = os.path.dirname(__file__)
 
 # Leer credenciales
 ruta_info = os.path.join(ruta_base, '..', 'credenciales.inf')
 ruta_info = os.path.normpath(ruta_info)
+comprobar_credenciales(ruta_info)
 datos = leer_credenciales(ruta_info)
 
 EMAIL = datos.get('email')
@@ -33,20 +49,18 @@ ruta_html = os.path.join(ruta_base, '..', 'html', nombre_archivo)
 with open(ruta_html, 'r', encoding='utf-8') as f:
     html_content = f.read()
 
-
-
-# Reemplazar {{nombre}} por el nombre personalizado
+# Reemplaza {{nombre}} por el nombre personalizado
 nombre_personalizado = "nombre"
 html_personalizado = html_content.replace('{{nombre}}', nombre_personalizado)
 
-# Preparar correo
+# Preparacion del correo
 mensaje = MIMEMultipart('alternative')
-mensaje['Subject'] = 'Correo HTML embebido 🐍'
+mensaje['Subject'] = 'Correo HTML'
 mensaje['From'] = EMAIL
 mensaje['To'] = 'mail@mail.com'
 mensaje.attach(MIMEText(html_personalizado, 'html'))
 
-# Enviar
+# Envio
 with smtplib.SMTP('smtp.office365.com', 587) as servidor:
     servidor.starttls()
     servidor.login(EMAIL, APP_PASSWORD)
